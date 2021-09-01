7f34e0407c6f9ad6d3c90260feaf4133c411fc93
sandeepkiranjada <36130965+sandeepkiranjada@users.noreply.github.com>
2021-09-01 10:17:08 -0400

    v

---
+++ Matlab/Jada/New folder/n.m
@@ -0,0 +1,254 @@
+clear
+clc
+close all
+
+%% Model
+
+M = 250; % Vehicle Mass in kgs
+m = 10; % Wheel Mass in kgs
+
+k1 = 150e3; % Passive Stiffness in N/m
+b1 = 1e3;  % Passive Damping in N/(m/s)
+
+k2 = 10e3; % Tire Stiffness in N/m
+b2 = 0.1e3;  % Tire Damping in N/(m/s)
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
---
+++ Matlab/Jada/New folder/nm.m
@@ -0,0 +1,251 @@
+
+W0 = 10; % Process noise covariance, for wheel vertical speed, in (N/kg)^2
+V = 1e-8; % Measurement noise covariance,
+          % for distance between wheel and vehicle, in (m)^2
+
+Gamma = [0 0 0 1]';
+
+W = Gamma*W0*Gamma';
+
+%% State-Space Represetation
+
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
+तत्रैतत्स्यात्सातत्येन रसप्राप्तावारम्भकाले मध्यस्थचि त्तता
+नातिसहि ष्णुता च । तत ः क्रमेणाधिको रागयोगः शरीरे
+नि रपेक्षत्वमन्ते च वि रामाभीप्सेत्येतदपु पन्नमि ति ॥ २० ॥
+Here one might object, “It is assumed that the woman
+experiences sensual pleasure continually, yet it is evident
+that, at the beginning, she is indifferent and has no
+endurance. Then, sexual passion and indifference to the
+body gradually increase, and at the end, she wants to stop.”
+तच्च न । सामान्येऽपि भ्रान्त िसंस्कारे कुलालचक्रस्य भ्रमरकस्य वा
+भ्रान्तावेव वर्त मानस्य प्रारम्भे मन्दवेगता तत श्च क्रमेण पूरणं वेग-
+स्येत्युपपद्यते । धातुक्षयाच्च वि रामाभीप्सेति । तस् मादनाक्षेपः ॥ २१ ॥
+But this is not so. Even if a potter’s wheel or a spinning
+top starts to spin in the same manner, when they are
+spinning they are slow at the beginning, but then
+gradually pick up speed. This is perfectly clear. And the
+wish to stop is due to the loss of fluids. Therefore, this
+is not a relevant objection.
+सुरतान्ते सुखं पुंसां स्त्रीणां तु सतत ं सुखम् ।
+धातुक्षयनिमि त्ता च वि रामेच्छोपजायते ॥ २२ ॥
+Men’s pleasure comes at the end of the sexual act, whereas
+the pleasure of women is continuous, and the wish to stop
+is due to the loss of fluids.
